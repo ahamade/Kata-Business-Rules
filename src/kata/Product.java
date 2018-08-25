0a4848057877575f81f02a5f6f27f58a40c2e941
Ammar Hamadeh <ammar.hamadeh@gmail.com>
2018-08-25 15:30:50 -0400

    Initial project packages

---
+++ src/kata/Product.java
@@ -0,0 +1,19 @@
+package kata;
+
+public class Product {
+	
+	private String type;
+
+	public Product(String type) {
+		this.type = type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+}
